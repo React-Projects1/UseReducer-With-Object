93a13ab3d875d4ec161bd7bf07dfbfb242bfefb5
Marwa Refaieh <marwarefaieh@gmail.com>
2024-10-09 16:15:45 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useReducer } from "react";
+import "./App.css";
+import { type } from "@testing-library/user-event/dist/type";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const initialState = {
+    num1 : 0,
+    num2 : 0
+  };
+
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "incNum1":
+        return {...state , num1: state.num1 + action.value };
+      case "incNum2":
+        return {...state , num2: state.num2 + action.value };
+      default:
+        return state;
+    }
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  return <div className="App">
+    <h2>{state.num1}</h2>
+    <button onClick={()=>{dispatch({type:'incNum1' , value: 10})}}>Num1 +</button>
+    <h2>{state.num2}</h2>
+    <button onClick={()=>{dispatch({type:'incNum2' , value: 20})}}>Num2 +</button>
+  </div>;
 }
 
 export default App;
